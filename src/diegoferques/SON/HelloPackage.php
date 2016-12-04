6eda64c0a0878dbc5757cbec4eb64e75ceb10b81
diegoferques <diegoferques@gmail.com>
2016-12-04 01:08:49 -0200

    Primeiro Commit pelo próprio site do GIT

---
+++ src/diegoferques/SON/HelloPackage.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace diegoferques\SON;
+
+/**
+ * Description of HelloPackage
+ *
+ * @author matto
+ */
+class HelloPackage {
+    //put your code here
+    public function getHello(){
+        return "Hello Package";
+    }
+}
